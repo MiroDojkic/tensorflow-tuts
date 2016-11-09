31dcc382d954b9153c9a4af2a85155690678337c
Miroslav Dojkic <mdojkic@gmail.com>
2016-11-09 09:55:45 +0100

    Refactored inference, temp disabled dropout

--- deepMNIST.py
+++ deepMNIST.py
@@ -30,12 +30,7 @@ def max_pooling(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-def main(_):
-    data_sets = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-    sess = tf.InteractiveSession()
-
-    x, y_ = create_input_placeholders(number_of_features=784, number_of_labels=10)
-
+def infer(x):
     W_conv1 = generate_weights([5, 5, 1, 32])
     b_conv1 = generate_biases([32])
     x_image = tf.reshape(x, [-1, 28, 28, 1])
@@ -55,29 +50,39 @@ def main(_):
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    # keep_prob = tf.placeholder(tf.float32)
+    # h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     W_fc2 = generate_weights([1024, 10])
     b_fc2 = generate_biases([10])
 
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
+    return y_conv
 
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+
+def main(_):
+    data_sets = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+    sess = tf.InteractiveSession()
+
+    x, y_ = create_input_placeholders(number_of_features=784, number_of_labels=10)
+
+    y = infer(x)
+
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     sess.run(tf.initialize_all_variables())
     for i in range(20000):
         batch = data_sets.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(feed_dict={
-                x: batch[0], y_: batch[1], keep_prob: 1.0})
+                x: batch[0], y_: batch[1]})
             print("step %d, training accuracy %g" % (i, train_accuracy))
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+        train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
     print("test accuracy %g" % accuracy.eval(feed_dict={
-        x: data_sets.test.images, y_: data_sets.test.labels, keep_prob: 1.0}))
+        x: data_sets.test.images, y_: data_sets.test.labels}))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
